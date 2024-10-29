9397da0defeececd75f0d48c23ea21d5341cee13
AleFunky <55950650+AleFunky@users.noreply.github.com>
2024-10-29 19:52:48 +0100

    mercadona 1

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from mercapy import Mercadona
 
-mercadona = Mercadona("14001")
-busqueda = mercadona.search(input("Introduzca el producto a buscar: "))
-if len(busqueda) == 0:
-    print("No se ha encontrado nada.")
-else:
-    for producto in busqueda:
-        print("--------", producto.id, "------")
-        print("Nombre:", producto.name)
-        print("Descripción:", producto.description)
-        print("Marca:", producto.brand)
-        print("Precio:", producto.unit_price, "€")
-        if producto.is_new:
-            print("Novedad: Sí")
+mercadona = Mercadona("14010")
+print("País del Mercadona elegido:", mercadona.language)
+aBuscar = ""
+while aBuscar != "*":
+    aBuscar = input("Introduzca el producto a buscar (* para salir):\n")
+    if aBuscar != "*":
+        busqueda = mercadona.search(aBuscar)
+        longitud = len(busqueda)
+        print("Resultados:", longitud)
+
+        if longitud == 0:
+            print("No se ha encontrado nada.")
         else:
-            print("Novedad: No")
+            if input("¿Continuar? (Y/N): ").lower() == "y":
+                encabezado = ""
+                contador = 0
+                for producto in busqueda:
+                    contador += 1
+                    encabezado = f"--------{producto.id} ({contador}/{longitud})--------"
+                    print(encabezado)
+                    print("Nombre:", producto.name)
+                    print("Descripción:", producto.description)
+                    print("Marca:", producto.brand)
+                    print("Precio:", producto.unit_price, "€")
+                    if producto.is_new:
+                        print("Novedad: Sí")
+                    else:
+                        print("Novedad: No")
+
+                print("-" * len(encabezado))
+    else:
+        print("Saliendo...")
